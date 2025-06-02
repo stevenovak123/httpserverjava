7d1c9da23d14c5053b924c01d5f8fbfa6a38e500
stevenovak123 <stevenovak0802@gmail.com>
2025-06-02 14:37:48 +0100

    Added HttpConfigurationException and implemented in ConfigurationManager

--- src/main/java/com/steve/config/ConfigurationManager.java
+++ src/main/java/com/steve/config/ConfigurationManager.java
@@ -1,9 +1,11 @@
 package com.steve.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.steve.util.Json;
 
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -28,15 +30,34 @@ public class ConfigurationManager {
 /**
 * Used to load a configuration File by the path provided
  */
-    public void loadConfigurationFile(String filePath) throws IOException {
-        FileReader fileReader= new FileReader(filePath);
+    public void loadConfigurationFile(String filePath)  {
+        FileReader fileReader= null;
+        try {
+            fileReader = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            throw new HttpConfigurationException(e);
+        }
         StringBuffer buffer = new StringBuffer();
         int i;
-        while( (i= fileReader.read() ) !=-1){
+        while(true){
+            try {
+                if (!((i= fileReader.read() ) !=-1)) break;
+            } catch (IOException e) {
+                throw new HttpConfigurationException(e);
+            }
             buffer.append((char)i);
         }
-        JsonNode contents = Json.parse(buffer.toString());
-        myCurrentConfiguration= Json.fromJson(contents, Configuration.class);
+        JsonNode contents = null;
+        try {
+            contents = Json.parse(buffer.toString());
+        } catch (JsonProcessingException e) {
+            throw new HttpConfigurationException("Error parsing the Configuration File contents",e);
+        }
+        try {
+            myCurrentConfiguration= Json.fromJson(contents, Configuration.class);
+        } catch (JsonProcessingException e) {
+            throw new HttpConfigurationException("Error parsing the Configuration File internally",e);
+        }
 
     }
     /**
@@ -45,8 +66,11 @@ public class ConfigurationManager {
      *
      */
 
-    public void getCurrentConfiguration(){
-
+    public Configuration getCurrentConfiguration(){
+if(myCurrentConfiguration==null){
+    throw new HttpConfigurationException("No Current configuration present");
+}
+return myCurrentConfiguration;
     }
 }
 
