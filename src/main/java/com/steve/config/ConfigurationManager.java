17596ab448fa3b3c4fba570c0b82013440b5ae55
stevenovak123 <stevenovak0802@gmail.com>
2025-06-02 14:31:52 +0100

    Updating ConfigurationManager.java for file reading

--- src/main/java/com/steve/config/ConfigurationManager.java
+++ src/main/java/com/steve/config/ConfigurationManager.java
@@ -1,4 +1,12 @@
 package com.steve.config;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.steve.util.Json;
+
+
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * We are using a singleton pattern
  * Single manager shared through out the project
@@ -20,12 +28,22 @@ public class ConfigurationManager {
 /**
 * Used to load a configuration File by the path provided
  */
-    public void loadConfigurationFile(String filePath){
+    public void loadConfigurationFile(String filePath) throws IOException {
+        FileReader fileReader= new FileReader(filePath);
+        StringBuffer buffer = new StringBuffer();
+        int i;
+        while( (i= fileReader.read() ) !=-1){
+            buffer.append((char)i);
+        }
+        JsonNode contents = Json.parse(buffer.toString());
+        myCurrentConfiguration= Json.fromJson(contents, Configuration.class);
 
     }
     /**
-     *     Returns the current loaded configuration
-     * */
+     * Returns the current loaded configuration
+     *
+     *
+     */
 
     public void getCurrentConfiguration(){
 
